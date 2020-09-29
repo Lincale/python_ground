65c756818a0c20e6ed043bcdddc405bda14432a2
Lincale <Linca.github@gmail.com>
2020-09-30 01:04:57 +0900

    commonらしいこと書いた（ほんとか？）

--- ground/common.py
+++ ground/common.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 def print_method(*args):
     print(*args)
+
+
+def isNotZero(num):
+    if not num:
+        return True
+    else:
+        return False
+
+
+def isZero(num):
+    if num:
+        return True
+    else:
+        return False
--- ground/logic.py
+++ ground/logic.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from common import print_method
+from common import print_method, isZero
 
 
 class SampleClass:
-    def __init__(self, hoge, fuga):
+    def __init__(self, hoge, fuga, height, width):
         self.hoge = hoge
         self.fuga = fuga
+        self.height = height
+        self.width = width
 
     def print_hoge(self):
         print_method(self.hoge)
@@ -16,9 +18,14 @@ class SampleClass:
     def print_hoge_and_fuga(self):
         print_method(self.hoge, self.fuga)
 
+    def square_area(self):
+        if isZero(self.height) and isZero(self.width):
+            return self.height * self.width
+
 
 if __name__ == "__main__":
-    sampleClass = SampleClass('hoge', 'fuga')
+    sampleClass = SampleClass('hoge', 'fuga', 2, 4)
     sampleClass.print_hoge()
     sampleClass.print_fuga()
     sampleClass.print_hoge_and_fuga()
+    print_method(sampleClass.square_area())
